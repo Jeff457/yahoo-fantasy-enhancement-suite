57194f2129b53ad8327d112fd65507244cbd547f
Jeff Stanton <jstanton397@yahoo.com>
2021-10-03 18:56:55 -0700

    added support for embedding youtube videos

--- fantasy-msg-board-embed-content/content.js
+++ fantasy-msg-board-embed-content/content.js
@@ -3,20 +3,48 @@ const CONTAINER_STYLE =
   "height: 0; padding-bottom: calc(55.21%); position:relative; width: 100%;";
 const IMG_STYLE =
   "border:0; height:100%; left:0; overflow:hidden; position:absolute; top:0; width:100%";
+const YOUTUBE_EMBEDDED = "https://www.youtube.com/embed/";
 const CONTENT_RE = /\.(jpg|jpeg|png|gif)$/i;
+const YOUTUBE_RE = /^.*(youtube\.com\/watch\?v=)([^#\&\?]*).*/;
 
-const createEmbeddedContentFor = (contentSrc) => {
+const embedMediaInContainer = (mediaContainer) => {
   const embeddedContainer = document.createElement("div");
   embeddedContainer.style = CONTAINER_STYLE;
+  embeddedContainer.appendChild(mediaContainer);
 
+  return embeddedContainer.outerHTML;
+};
+
+const createEmbeddedImg = (contentSrc) => {
   const img = document.createElement("img");
   img.src = contentSrc;
   img.style = IMG_STYLE;
   img.width = "100%";
 
-  embeddedContainer.appendChild(img);
+  return embedMediaInContainer(img);
+};
 
-  return embeddedContainer.outerHTML;
+const createEmbeddedYouTubeVideo = (url) => {
+  const videoId = url.split("?v=")[1];
+
+  const iFrame = document.createElement("iframe");
+  iFrame.src = YOUTUBE_EMBEDDED + videoId;
+  iFrame.width = "560";
+  iFrame.height = "315";
+  iFrame.title = "YouTube video player";
+  iFrame.allow =
+    "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+  iFrame.setAttribute("allowfullscreen", "");
+
+  return embedMediaInContainer(iFrame);
+};
+
+const isImage = (string) => {
+  return CONTENT_RE.test(string.split("?")[0]);
+};
+
+const isYouTubeVideo = (string) => {
+  return YOUTUBE_RE.test(string);
 };
 
 const embedContent = () => {
@@ -27,13 +55,12 @@ const embedContent = () => {
     const words = message.innerHTML.split(" ");
 
     words.forEach((word, i) => {
-      // I could see users leaving in query strings after copying the link address
-      const potentialContentSource = word.trim().split("?")[0];
-      if (CONTENT_RE.test(potentialContentSource)) {
-        const embeddedContent = createEmbeddedContentFor(
-          potentialContentSource
-        );
-        words[i] = embeddedContent;
+      const potentialContentSource = word.trim();
+      if (isImage(potentialContentSource)) {
+        // I could see users leaving in query strings after copying the link address
+        words[i] = createEmbeddedImg(potentialContentSource.split("?")[0]);
+      } else if (isYouTubeVideo(potentialContentSource)) {
+        words[i] = createEmbeddedYouTubeVideo(potentialContentSource);
       }
     });
 
